5abd625675236b9068438d90615fa08d45a389db
Felix Dörre <felix@dogcraft.de>
2014-12-03 21:07:15 +0100

    add: require the error messages in the X509Req-CSR test

--- test/src/X509Req.cpp
+++ test/src/X509Req.cpp
@@ -2,6 +2,8 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <openssl/err.h>
+
 #include "X509.h"
 #include "util.h"
 
@@ -12,16 +14,23 @@ BOOST_AUTO_TEST_CASE( CSR ) {
     std::shared_ptr<X509Req> req( X509Req::parseCSR( readFile( "testdata/test.csr" ) ) );
     BOOST_REQUIRE( req );
     BOOST_CHECK( req->verify() == 1 );
+    BOOST_REQUIRE( ERR_peek_error() == 0 );
 
     // Testing a CSR, where the signature content has been tampered with
     req = std::shared_ptr<X509Req>( X509Req::parseCSR( readFile( "testdata/test_false_sig.csr" ) ) );
     BOOST_REQUIRE( req );
     BOOST_CHECK( req->verify() == 0 );
+    BOOST_REQUIRE( ERR_get_error() != 0 ); // RSA_padding_check_PKCS1_type_1:block type is not 01
+    BOOST_REQUIRE( ERR_get_error() != 0 ); // RSA_EAY_PUBLIC_DECRYPT:padding check failed
+    BOOST_REQUIRE( ERR_get_error() != 0 ); // ASN1_item_verify:EVP lib
+    BOOST_REQUIRE( ERR_get_error() == 0 );
 
     // Testing a CSR, where the signature OID is something strange
     req = std::shared_ptr<X509Req>( X509Req::parseCSR( readFile( "testdata/test_invalid_sig.csr" ) ) );
     BOOST_REQUIRE( req );
     BOOST_CHECK( req->verify() < 0 );
+    BOOST_REQUIRE( ERR_get_error() != 0 ); // ASN1_item_verify:unknown signature algorithm
+    BOOST_REQUIRE( ERR_get_error() == 0 );
 }
 
 BOOST_AUTO_TEST_CASE( SPKAC ) {
