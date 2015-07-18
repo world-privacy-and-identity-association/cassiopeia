900e14f37a15fd743527147c7cb2e859b8dfb498
Benny Baumann <BenBE@geshi.org>
2015-07-18 17:39:48 +0200

    chg: Updated formatting command

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-astyle --style=java --add-brackets --indent-col1-comments --break-blocks --pad-oper --pad-paren-in --unpad-paren --indent-namespaces --align-pointer=type --align-reference=type --convert-tabs --lineend=linux -r "*.cpp" "*.h"
+astyle --style=java --add-brackets --indent-col1-comments --break-blocks --pad-oper --pad-paren-in --unpad-paren --indent-namespaces --align-pointer=type --align-reference=type --convert-tabs --lineend=linux -r "*.cpp" "*.h" '*.hpp'
