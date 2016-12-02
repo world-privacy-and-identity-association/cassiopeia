89016837dcbf2775cd15dc8cbaba00dc6379f86e
Felix Dörre <felix@dogcraft.de>
2016-12-02 11:31:58 +0000

    update openssl-update-script

--- scripts/fetchOpenssl.sh
+++ scripts/fetchOpenssl.sh
@@ -1,16 +1,17 @@
-!/bin/sh
+#!/bin/sh
 mkdir -p ../lib/openssl
 cd ../lib/openssl
 
-wget -O openssl.tar.gz https://www.openssl.org/source/openssl-1.0.1j.tar.gz
+wget -O openssl.tar.gz https://www.openssl.org/source/openssl-1.1.0c.tar.gz
 echo "Verifying sha1"
-[ "cff86857507624f0ad42d922bb6f77c4f1c2b819" = "$(sha1sum "openssl.tar.gz"|cut '-d ' -f1)" ] || exit 2
+[ "920e6e7bdaccd94d7564af1097176f11900d20ca" = "$(sha1sum "openssl.tar.gz"|cut '-d ' -f1)" ] || exit 2
 
 echo "Verifying sha512"
-[ "a786bb99b68d88c1de79d3c5372767f091ebeefb5abc1d4883253fd3ab5a86af53389f5ff36fdd8faa27c5fb78be8bbff406392c373358697da80d250eadebb8" = "$(sha512sum "openssl.tar.gz"|cut '-d ' -f1)" ] || exit 2
+[ "e3cfba6c682e5edd6f678df7c1da9c9713880f7dca248e6d62f095185c22ce8fd7571d53a54a119fb5d4422578637746ad2809bb2ba324a5c54564f532307ad9" = "$(sha512sum "openssl.tar.gz"|cut '-d ' -f1)" ] || exit 2
 
 tar xf openssl.tar.gz
 mv openssl-*/* .
+mv openssl-*/.* .
 rmdir openssl-*
 
 rm openssl.tar.gz
