205cff4fcae1f8fcc8d99ab405736ea5b9f2c52a
Felix Dörre <felix@dogcraft.de>
2016-12-15 10:14:38 +0100

    upd: cleanups; 'std::swap' and '= {}'
    Change-Id: Iec5605a0618debb539aef1924b02e4ccfb444f8d

--- src/crypto/CRL.cpp
+++ src/crypto/CRL.cpp
@@ -139,12 +139,8 @@ void CRL::setSignature( std::string signature ) {
     ASN1_BIT_STRING *sig = d2i_ASN1_BIT_STRING( NULL, &buffer, signature.size() + data - buffer );
     ASN1_TIME *a1 = d2i_ASN1_TIME( NULL, &buffer, signature.size() + data - buffer );
     ASN1_TIME *a2 = d2i_ASN1_TIME( NULL, &buffer, signature.size() + data - buffer );
-    auto tmp = *palg;
-    *palg = *alg;
-    *alg = tmp;
-    auto tmp2 = *psig;
-    *psig = *sig;
-    *sig = tmp2;
+    std::swap(*palg, *alg);
+    std::swap(*psig, *sig);
     X509_CRL_set1_lastUpdate( crl.get(), a1);
     X509_CRL_set1_nextUpdate( crl.get(), a2);
 
